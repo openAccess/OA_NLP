9333d1bc5d047e8428ab8eeee4b9aec21aa7571e
Bill OConnor <wtoconnor@gmail.com>
2012-01-01 11:13:18 -0800

    Restructured corpus for compatibility with nltk readers.

--- openAccess/gitPLoS/nltk/corpus/builder.py
+++ openAccess/gitPLoS/nltk/corpus/builder.py
@@ -1,85 +1,39 @@
 '''
 '''
-import re
-'''
-    These are default stop words. Selected by looking at
-    corpus results. Probably should turn this into a hash
-    for speed.
-'''
-_defaultStopWords = ('', '=', 
-                     'a', 'an', 'as', 'at', 'are','also', 'and', 
-                     'be', 'by', 'but',
-		     'can', 'could', "can't", 
-		     'do', 'did', 'done',
-		     'e.g', 'eq',
-		     'for', 'fig', 'from',
-		     'go', 'gone', 'get',
-		     'has', 'had', 'have',
-		     'i.e', 'in', 'is', 'it', 'its', 'into',
-		     'my', 'mine', 'may', 'maynot',
-		     'no', 'not', 
-		     'of', 'on', 'or',
-                     'to', 'than', 'then', 'the', 'this',
-		     'so', 'soon',
-                     'we', 'went', 'with', 'within', 'when', 'where', 'were', 'would',
-		     "wouldn't",
-		    )
+from __future__ import division
+import os, re, nltk, pprint, json
+from datetime import datetime
 
 # These fields are required for the corpus
-ReturnFields = 'id,journal,author,subject,abstract,body'
-
-def _wsTokenize( s ):
-    '''
-    Split on whitespace and remove blanks.
-    '''
-    l = s.split()
-    return [ t.lower() for t in l if t is not '']
-
-def _count( l ):
-    '''
-    Make a count of unique words.
-    '''
-    counts = {}
-    for w in l:
-        counts[w] = counts[w] + 1 if w in counts else 1
-    return counts
-
-def _rmPunc( l ):
-    '''
-    Some of the words begin or end with punctuatuion.
-    '''
-    return [ w.rstrip(' -,;?.:)(').lstrip(' -,;?.:)(') for w in l]
-
-def _rmRefNums( l ):
-    '''
-    Article body has many references of the form [d].
-    '''
-    pat = re.compile(r'''\[\d+\]''')
-    return [ w for w in l if not pat.match(w)]
-
-def _rmStopWords( stop_words, l ):
-    '''
-    Remove words from the stop word list.
-    '''
-    return [ w for w in l if w not in stop_words]
+ReturnFields = 'id,journal,publication_date,author,subject,title,abstract,body'
 
 class Builder():
 
-    def __init__(self, stop_words = _defaultStopWords) :
-        self.stop_words = stop_words
+    def __init__(self, query, corpus_name='myCorpus') :
+	self.corpus_info = {}
+        self.corpus_info['name'] = corpus_name
+	self.corpus_info['creation_date'] = datetime.now().isoformat()
+	self.corpus_info['query'] = query
+	self.corpus_name = corpus_name
+	self.fileids = []
+	self.corpus_info['fileids'] = self.fileids
+	os.mkdir( corpus_name )
         return
 
-    def build(self, doc):
-	wl = _wsTokenize(doc['abstract'][0])
-	wl = _rmPunc( wl )
-	doc['abstract_wl'] = _rmStopWords( self.stop_words, wl )
-	doc['abstract_cnt'] = _count( doc['abstract_wl'] )
-
-	wl = _wsTokenize(doc['body'])
-	wl = _rmPunc( wl )
-	wl = _rmRefNums( wl )
-	doc['body_wl']  = _rmStopWords( self.stop_words, wl )
-	doc['body_cnt'] = _count( doc['body_wl'] )
-	return doc
-
-    
+    def build(self, doc_list, verbose=False):
+	'''
+        Create a json file for each doc returned by the query.
+	Then create a corpus info file.
+	'''
+        for doc in doc_list:
+            fn = doc['id'].replace('/','-') + '.json'
+	    fd = open( self.corpus_name + '/' + fn, 'w')
+	    json.dump( doc, fd, indent=5 )
+	    if verbose: print 'Added : ' + fn
+	    fd.close()
+            self.fileids.append(fn)
+	# Output the corpus info
+        fd = open( self.corpus_name + '/corpus_info.json', 'w')
+	json.dump( self.corpus_info, fd, indent=5 )
+	fd.close()
+	return
