9312f33ffba66ebd26985ed22d9b606745b4eb73
Bill OConnor <wtoconnor@gmail.com>
2012-01-18 11:31:28 -0800

    Went backt to shared util.py. Fixed other bugs.

--- openAccess/gitPLoS/nltk/corpus/reader.py
+++ openAccess/gitPLoS/nltk/corpus/reader.py
@@ -21,33 +21,8 @@
     in nltk.readers.
 """
 import json
+from util import doi2fn, listafy
 from nltk.corpus.reader.plaintext import  CategorizedPlaintextCorpusReader
-
-def _doi2fn(doi, doc_part):
-    """
-    Convert a DOI to a file name.
-
-    @type doi: string or list
-    @param doi: Digital Object Identifier of the article in the corpus.
-
-    @rtype: string or list
-    @return: Returns a single file name or a list of filenames..
-    """
-    fname = lambda d: '%s-%s.txt' % ( d.replace('/', '-'), doc_part)
-    if isinstance(doi, basestring): 
-        return fname(doi)
-    else:
-        return [ fname(d) for d in doi ]
-
-def _listafy(obj, aDict):
-    """
-    """
-    if obj == None:
-        return aDict.keys()
-    elif isinstance(obj, basestring):
-        return [ obj ]
-    else:
-        return obj
 
 class PlosReader(CategorizedPlaintextCorpusReader):
     """
@@ -81,15 +56,20 @@ class PlosReader(CategorizedPlaintextCorpusReader):
 	# 'abstract' and 'body' are currently supported.
 	# The corpus contains seperate text for each, but the 
 	# reader is initialized to readi only one.
-        doc_part = kwargs['doc_part']  if 'doc_part' in kwargs else 'body'
-        self._doc_part = doc_part
-        fileids = [ _doi2fn(d, doc_part) for d in info['d2c'].keys() ] if 'fileids' not in kwargs else kwargs['fileids']
-
+	if 'doc_part' in kwargs:
+            self._doc_part = doc_part = kwargs['doc_part']
+	    del kwargs['doc_part']
+	else:
+	    delf._doc_part = doc_part = 'body'
+	if 'fileids' not in kwargs:
+            fileids = [ doi2fn(d, doc_part) for d in info['d2c'].keys() ] 
+        else:
+	    fileids =  kwargs['fileids']
         # cat_map f -> [ c1, c2, ...]
 	# The fileids depend on what the doc_part is ('body', 'abstract')
 	cat_map = {}
         for d,cat in info['d2c'].iteritems():
-            cat_map[_doi2fn(d, doc_part)] = cat
+            cat_map[doi2fn(d, doc_part)] = cat
 
 	kwargs['cat_map'] = cat_map
 	# Subclass of Categorized Plaintext Corpus Reader
@@ -97,34 +77,34 @@ class PlosReader(CategorizedPlaintextCorpusReader):
 
     def dois(self):
         """
-        """
+	"""
         return self._corpus_info['d2c'].keys()
 
     def articleURL(self, doi_lst=None):
         """
         """
-	dois = _listafy(doi_lst, self._corpus_info['d2c'])
+	dois = listafy(doi_lst, self._corpus_info['d2c'])
 	amap = self._corpus_info['article_link']
         return zip(dois, [ amap[d] for d in dois])
 
     def articleXML(self, doi_lst=None):
         """
         """
-	dois = _listafy(doi_lst, self._corpus_info['d2c'])
+	dois = listafy(doi_lst, self._corpus_info['d2c'])
 	xmap = self._corpus_info['xml_link']
         return zip(dois, [ xmap[d] for d in dois ])
 
     def doi2fid(self, doi_lst=None):
         """
         """
-        dois = _listafy(doi_lst, self._corpus_info['d2c']) 
-        return zip(dois, _doi2fn(dois, self._doc_part))
+        dois = listafy(doi_lst, self._corpus_info['d2c']) 
+        return zip(dois, doi2fn(dois, self._doc_part))
 
     def authors(self, doi_lst=None):
         """
         Build a list of (doi , author) tuples.
 	"""
-        dois = _listafy(doi_lst, self._corpus_info['d2c'])
+        dois = listafy(doi_lst, self._corpus_info['d2c'])
 	d2info = self._corpus_info['d2info']
 	alist = []
 	for d in dois:
@@ -135,29 +115,29 @@ class PlosReader(CategorizedPlaintextCorpusReader):
     def pub_date(self, doi_lst=None):
         """
         """
-        dois = _listafy(doi_lst, self._corpus_info['d2c'])
+        dois = listafy(doi_lst, self._corpus_info['d2c'])
 	d2info = self._corpus_info['d2info']
 	dlist = []
 	for d in dois:
-            (_,pd,_,_,_) = d2info[d]
+            (_,pd,_,_,_) = d2info[d] if d in d2info else (0,None,0,0,0) 
 	    dlist.extend((d, pd))
         return dlist
 
     def article_type(self, doi_lst=None):
         """
         """
-        dois = _listafy(doi_lst, self._corpus_info['d2c'])
+        dois = listafy(doi_lst, self._corpus_info['d2c'])
 	d2info = self._corpus_info['d2info']
 	alist = []
 	for d in dois:
-            (_,_,atype,_,_) = d2info[d]
+            (_,_,atype,_,_) = d2info[d] if d in d2info else (0,0,None,0,0) 
 	    alist.extend((d, atype))
         return alist
 
     def title(self, doi_lst=None):
         """
         """
-        dois = _listafy(doi_lst, self._corpus_info['d2c'])
+        dois = listafy(doi_lst, self._corpus_info['d2c'])
 	d2info = self._corpus_info['d2info']
 	tlist = []
 	for d in dois:
